5861cd72e78277b23c94c558047b37d1c4730854
hilfa vt <hilfavt007@gmail.com>
2022-07-12 12:52:28 +0530

    changes

--- todo/settings.py
+++ todo/settings.py
@@ -15,8 +15,6 @@ import os
 import django_heroku
 
 
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,8 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
+STATIC_URL = '/static/' 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
